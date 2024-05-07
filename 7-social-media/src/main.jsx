61bddb8a9abea9c9121301d831f8e4a80791cebd
rkanand007 <ramkrishananand7@gmail.com>
2024-05-07 17:59:44 +0530

    React Router Dom

--- 7-social-media/src/main.jsx
+++ 7-social-media/src/main.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import CreatePost from "./components/CreatePost.jsx";
 
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/create-post", element: <CreatePost /> },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
